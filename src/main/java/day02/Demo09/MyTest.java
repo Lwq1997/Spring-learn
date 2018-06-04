635c4a10907a35f27a14ec0b4c0c4c7fb980a490
Lwq <liwanqi@etouch.cn>
2018-06-04 15:08:58 +0800

    使用同类抽象Bean注入。增加abstrate属性，让抽象的bean不能直接get到

--- src/main/java/day02/Demo09/MyTest.java
+++ src/main/java/day02/Demo09/MyTest.java
@@ -16,6 +16,8 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("day02/Demo09/applicationContext.xml");
         Student student1 = (Student) context.getBean("mystudent1");
         Student student2 = (Student) context.getBean("mystudent2");
+//        Student baseStudent = (Student) context.getBean("baseStudent");   //这个抽象的不能被直接拿过来
+
         System.out.println(student2);
         System.out.println(student1);
     }
