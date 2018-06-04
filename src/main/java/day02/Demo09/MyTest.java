848b17a1e7b019806c16cf1f72c5d68b6130a401
Lwq <liwanqi@etouch.cn>
2018-06-04 15:06:15 +0800

    使用同类抽象Bean注入

---
+++ src/main/java/day02/Demo09/MyTest.java
@@ -0,0 +1,22 @@
+package day02.Demo09;
+
+import day02.Demo07.Person;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author etouch
+ * @create 2018-06-01 15:15
+ * @desc
+ **/
+public class MyTest {
+    @Test
+    public void test01() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("day02/Demo09/applicationContext.xml");
+        Student student1 = (Student) context.getBean("mystudent1");
+        Student student2 = (Student) context.getBean("mystudent2");
+        System.out.println(student2);
+        System.out.println(student1);
+    }
+}
---
+++ src/main/java/day02/Demo09/Student.java
@@ -0,0 +1,40 @@
+package day02.Demo09;
+
+
+/**
+ * @author etouch
+ * @create 2018-06-01 18:49
+ * @desc
+ **/
+public class Student {
+    private String name;
+    private int age;
+    private String school;
+    private String department;
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", school='" + school + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
