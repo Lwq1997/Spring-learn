3d1d55e08a9d1284e88ddef05d3109b9ed2a6827
Lwq <liwanqi@etouch.cn>
2018-06-07 09:47:18 +0800

    方法名匹配切入点顾问

---
+++ src/main/java/day04/Demo07/MyAfterReturningAdvice.java
@@ -0,0 +1,23 @@
+package day04.Demo07;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author etouch
+ * @create 2018-06-06 18:41
+ * @desc
+ **/
+//后置通知
+public class MyAfterReturningAdvice implements AfterReturningAdvice {
+    //在目标方法执行之前执行
+    //method：目标方法
+    //objects：方法参数
+    //target ： 目标对象
+    //returnValue:返回值
+    @Override
+    public void afterReturning(Object returnValue, Method method, Object[] objects, Object target) throws Throwable {
+        System.out.println("我是后置方法");
+    }
+}
---
+++ src/main/java/day04/Demo07/SomeServiceImpl.java
@@ -0,0 +1,23 @@
+package day04.Demo07;
+
+
+/**
+ * @author etouch
+ * @create 2018-06-06 17:54
+ * @desc
+ **/
+public class SomeServiceImpl implements ISomeService{
+    public void doFirst() {
+        System.out.println("执行主业务逻辑first");
+    }
+
+    public void doSecond() {
+        System.out.println("执行主业务逻辑second");
+    }
+
+    @Override
+    public void doThird() {
+        System.out.println("执行主业务逻辑third");
+    }
+
+}
