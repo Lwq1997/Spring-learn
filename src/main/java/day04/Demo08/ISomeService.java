f7a141cd3db8d47e271c0749dd923a6a79bdf9b7
Lwq <liwanqi@etouch.cn>
2018-06-07 09:54:48 +0800

    正则表达式匹配全限定名切入点顾问

---
+++ src/main/java/day04/Demo08/ISomeService.java
@@ -0,0 +1,12 @@
+package day04.Demo08;
+
+/**
+ * @author etouch
+ * @create 2018-06-07 9:37
+ * @desc
+ **/
+public interface ISomeService {
+    public void doFirst();
+    public void doSecond();
+    public void doThird();
+}
---
+++ src/main/java/day04/Demo08/MyTest.java
@@ -0,0 +1,23 @@
+package day04.Demo08;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author etouch
+ * @create 2018-06-01 15:15
+ * @desc
+ **/
+public class MyTest {
+    @Test
+    public void test01() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/day04/Demo08/applicationContext.xml");
+        ISomeService service = (ISomeService) context.getBean("serviceProxy");
+        service.doFirst();
+        System.out.println("-----------------");
+        service.doSecond();
+        System.out.println("------------------");
+        service.doThird();
+    }
+}
